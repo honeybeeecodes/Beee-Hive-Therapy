394e93989d16602c023a6015e1c70d9617c860fa
honeybeeecodes <bianchivaldez@yahoo.com>
2022-10-11 18:46:31 -0400

    double typo

--- models/hive.js
+++ models/hive.js
@@ -1,11 +1,11 @@
 
 
 const mongoose = require('mongoose');
+
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
 
-const Schema = mongoose.Schema;
 	
 const hiveSchema = new Schema({
   title: String,
